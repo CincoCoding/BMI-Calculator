a1f7b81a020170bed3ddbbf4af230fa0ee7aa8c7
CincoCoding <coderdavidgarcia@gmail.com>
2023-02-12 03:26:17 -0600

    created planet button templates

--- lib/screens/input_page.dart
+++ lib/screens/input_page.dart
@@ -8,9 +8,18 @@ import '../constants.dart';
 import '../components/counter_card.dart';
 import '../components/BottomButton.dart';
 
-enum Gender {
-  male,
-  female,
+enum CelestialObject {
+  sun,
+  mercury,
+  venus,
+  earth,
+  moon,
+  mars,
+  jupiter,
+  saturn,
+  uranus,
+  neptune,
+  blackHole
 }
 
 class InputPage extends StatefulWidget {
@@ -19,7 +28,7 @@ class InputPage extends StatefulWidget {
 }
 
 class _InputPageState extends State<InputPage> {
-  Gender selectedGender;
+  CelestialObject selectedCelestialObject;
   int kAge = 27;
   int weight = 95;
   int height = 178;
@@ -51,22 +60,43 @@ class _InputPageState extends State<InputPage> {
                     child: Row(
                   children: <Widget>[
                     ReusableCard(
-                      color: Colors.black,
+                      onPress: () {
+                        setState(() {
+                          selectedCelestialObject = CelestialObject.mercury;
+                        });
+                      },
+                      color: selectedCelestialObject == CelestialObject.mercury
+                          ? kActiveCardColor
+                          : kInactiveCardColor,
+                      cardChild: HeavenlyBodyCard(
+                          celestialObject: "MERCURY",
+                          image: "icons/icons8-planet-60.png"),
+                    ),
+                    ReusableCard(
+                      onPress: () {
+                        setState(() {
+                          selectedCelestialObject = CelestialObject.venus;
+                        });
+                      },
+                      color: selectedCelestialObject == CelestialObject.venus
+                          ? kActiveCardColor
+                          : kInactiveCardColor,
                       cardChild: HeavenlyBodyCard(
                           celestialObject: "VENUS",
                           image: "icons/icons8-planet-60.png"),
                     ),
                     ReusableCard(
-                      color: Colors.black,
+                      onPress: () {
+                        setState(() {
+                          selectedCelestialObject = CelestialObject.earth;
+                        });
+                      },
+                      color: selectedCelestialObject == CelestialObject.earth
+                          ? kActiveCardColor
+                          : kInactiveCardColor,
                       cardChild: HeavenlyBodyCard(
-                          celestialObject: "VENUS",
-                          image: "icons/icons8-planet-60.png"),
-                    ),
-                    ReusableCard(
-                      color: Colors.black,
-                      cardChild: HeavenlyBodyCard(
-                          celestialObject: "VENUS",
-                          image: "icons/icons8-planet-60.png"),
+                          celestialObject: "EARTH",
+                          image: "icons/icons8-earth-128(-xxxhdpi).png"),
                     ),
                   ],
                 ))
@@ -76,151 +106,3 @@ class _InputPageState extends State<InputPage> {
     );
   }
 }
-
-// [
-// Expanded(
-// child: Row(children: <Widget>[
-// Expanded(
-// child: ReusableCard(
-// onPress: () {
-// setState(() {
-// selectedGender = Gender.male;
-// });
-// },
-// color: selectedGender == Gender.male
-// ? kActiveCardColor
-//     : kInactiveCardColor,
-// cardChild: GenderCard(
-// gender: "MALE",
-// icon: FontAwesomeIcons.mars,
-// ),
-// ),
-// ),
-// Expanded(
-// child: ReusableCard(
-// onPress: () {
-// setState(() {
-// selectedGender = Gender.female;
-// });
-// },
-// color: selectedGender == Gender.female
-// ? kActiveCardColor
-//     : kInactiveCardColor,
-// cardChild: GenderCard(
-// gender: 'FEMALE',
-// icon: FontAwesomeIcons.venus,
-// ),
-// ),
-// ),
-// ]),
-// ),
-// Expanded(
-// child: ReusableCard(
-// color: kCardColor,
-// cardChild: Column(
-// mainAxisAlignment: MainAxisAlignment.center,
-// children: <Widget>[
-// Text(
-// "HEIGHT",
-// style: kLabelTextStyle,
-// ),
-// Row(
-// crossAxisAlignment: CrossAxisAlignment.baseline,
-// mainAxisAlignment: MainAxisAlignment.center,
-// textBaseline: TextBaseline.alphabetic,
-// children: [
-// Text(
-// height.toString(),
-// style: kBigNumber,
-// ),
-// Text("cm", style: kLabelTextStyle),
-// ],
-// ),
-// SliderTheme(
-// data: SliderTheme.of(context).copyWith(
-// inactiveTrackColor: Color(0xFFBDBE98),
-// activeTrackColor: Colors.white,
-// thumbColor: Color(0xFFEB1555),
-// overlayColor: Colors.blue,
-// thumbShape:
-// RoundSliderThumbShape(enabledThumbRadius: 15),
-// overlayShape:
-// RoundSliderOverlayShape(overlayRadius: 20),
-// ),
-// child: Slider(
-// value: height.toDouble(),
-// min: 120.0,
-// max: 220.0,
-// // activeColor: Colors.red,
-// inactiveColor: Color(0xFF8D8E98),
-// onChanged: (double newValue) {
-// setState(() {
-// height = newValue.round();
-// });
-// },
-// ),
-// )
-// ],
-// ),
-// ),
-// ),
-// Expanded(
-// child: Row(
-// children: <Widget>[
-// Expanded(
-// child: CounterCardTwoButtons(
-// text: "WEIGHT",
-// textVariable: weight.toString(),
-// onPressed1: () {
-// setState(() {
-// weight--;
-// });
-// },
-// icon1: Icon(FontAwesomeIcons.minus),
-// onPressed2: () {
-// setState(() {
-// weight++;
-// });
-// },
-// icon2: Icon(FontAwesomeIcons.plus),
-// ),
-// ),
-// Expanded(
-// child: CounterCardTwoButtons(
-// text: "AGE",
-// textVariable: kAge.toString(),
-// onPressed1: () {
-// setState(() {
-// kAge--;
-// });
-// },
-// icon1: Icon(FontAwesomeIcons.minus),
-// onPressed2: () {
-// setState(() {
-// kAge++;
-// });
-// },
-// icon2: Icon(FontAwesomeIcons.plus),
-// ),
-// ),
-// ],
-// ),
-// ),
-// GestureDetector(
-// onTap: () {
-// CalculatorBrain calc =
-// CalculatorBrain(height: height, weight: weight);
-// Navigator.push(
-// context,
-// MaterialPageRoute(
-// builder: (context) => ResultsPage(
-// bmiResult: calc.calculateBMI(),
-// resultText: calc.getResult(),
-// interpretation: calc.getInterpretation(),
-// ),
-// ),
-// );
-// },
-// child: BottomButton(title: "CALCULATE"),
-// )
-// ]
